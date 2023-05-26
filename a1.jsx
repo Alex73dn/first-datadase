7c433bf7399f793af98b105644851954bb0a012d
Alex73dn <alex73dn@gmail.com>
2023-05-26 13:28:05 +0200

    changing a1

--- a1.jsx
+++ a1.jsx
@@ -7,3 +7,12 @@ var titleCaseOver;
 StudlyCapVar = 10;
 properCamelCase = "A String";
 var titleCaseOver = 9000;
+
+let a = 48;
+let b = 108;
+let c = 33;
+
+// Only change code below this line
+a /= 12;
+b /= 4;
+c /= 11;
