d2adc55b00a14c04e16e23d9dfe4a55ead6c1091
Alex73dn <alex73dn@gmail.com>
2023-06-08 21:06:39 +0200

    added es6

--- es6.js
+++ es6.js
@@ -1,2 +1,8 @@
 const myConcat = (arr1, arr2) => arr1.concat(arr2)
 console.log(myConcat([1, 2], [3, 4, 5])); // вззвращает значение всех эл-ов массива, здесь [ 1, 2, 3, 4, 5 ]
+
+const increment = (number, value=1) => number + value; //increase variable,preview value=1,если в консоль вывести (4,3)=7,то 1 игнорируется
+
+const sum = (...args) => {
+    return args.reduce((total, current) => total + current, 0);
+  }; // rest parameter - (...args)вызывает reduce -функцию обратного вызова (callback); total - аккумулятор,current-текущий эл-т массива
