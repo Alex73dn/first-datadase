566ed3c9eca9be9ace3a5ca6ec2fb35f6f602c23
Alex73dn <alex73dn@gmail.com>
2023-06-09 18:08:01 +0200

    template literal -

--- es6.js
+++ es6.js
@@ -13,3 +13,27 @@ const sum = (...args) => {
   } // диструкция убирает 2 первых элемента массива и функция возвращает некий массив без первых 2х элементов!!!
     const source = [1, 2, 3, 4, 5];
   const sourceWithoutFirstTwo = removeFirstTwo(source); // = [3,4,5]
+
+  const stats = {
+    max: 56.78,
+    median: 34.54,
+    min: -0.75,
+    average: 35.85
+  };
+  const half = ({ max, min }) => (max + min) / 2.0;
+  console.log(half(stats)); // Output: 28.015 создаём диструктивную функцию half, ср.значение amidst max и min
+
+  const result = {
+    success: ["max-length", "no-amd", "prefer-arrow-functions"],
+    failure: ["no-var", "var-on-top", "linebreak"],
+    skipped: ["no-extra-semi", "no-dup-keys"]
+  };
+  function makeList(arr) {
+   const failureItems = [];
+    for (let i = 0; i < arr.length; i++) {
+      const listItem = `<li class="text-warning">${arr[i]}</li>`; // template literal - ${arr[i]} в список li добавляет эл-ты
+      failureItems.push(listItem);
+    }
+    return failureItems;
+  }
+  const failuresList = makeList(result.failure); // функцией создаётся массив из элементов const result
