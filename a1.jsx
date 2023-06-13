f979ae4b71f0d845bf17a34ea41a153efe1db845
Alex73dn <alex73dn@gmail.com>
2023-06-13 17:32:11 +0200

    added props this

--- a1.jsx
+++ a1.jsx
@@ -105,3 +105,34 @@ class ToDo extends React.Component {
       }
     };
     
+    class CampSite extends React.Component {
+      constructor(props) {
+        super(props);
+      }
+      render() {
+        return (
+          <div>
+            <Camper />
+          </div>
+        );
+      }
+    }// CampSite рендерит  встроенный компонент <Camper />, 
+    class Camper extends React.Component {
+      constructor(props) {
+        super(props);
+      }
+      render() {
+        return (
+          <div>
+            <p>{this.props.name}</p>
+          </div>
+        );
+      }
+    }// Camper рендерит свойство имя
+    Camper.defaultProps = {
+      name: 'CamperBot'
+    };// имени присваевается дефолтное имя
+    Camper.propTypes = {
+      name: PropTypes.string.isRequired
+    };// имени присваевается свойство строка. 
+    
