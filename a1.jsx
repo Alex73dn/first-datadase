945cb6137115d01a0911d4ed9981983c0a975ba2
Alex73dn <alex73dn@gmail.com>
2023-06-16 16:57:31 +0200

    react added

--- a1.jsx
+++ a1.jsx
@@ -245,3 +245,34 @@ class MyForm extends React.Component {
     );
   }
 } // result кнопка submit с вводом и выводом напечатанного текста.
+
+class Results extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+     return <h1>{this.props.fiftyFifty ? 'You Win!' : 'You Lose!'}</h1>;
+  } // формируем функцию fiftyFifty, принимающее значение true/false (по умолчанию true) и если true - You Win! иначе You Lose!
+}
+class GameOfChance extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 1
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    this.setState(prevState => ({
+      counter: prevState.counter + 1
+    }));
+  }
+  render() {
+    const expression = Math.random() >= 0.5; // случайное значение 50/50 возвращает либо true либо false
+    return (
+      <div>
+        <button onClick={this.handleClick}>Play Again</button>
+        <Results fiftyFifty={expression} />
+        <p>{'Turn: ' + this.state.counter}</p> // менеся значение
+      </div>
+    ); }}
