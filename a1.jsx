63f7b01d97348b726a3a6ea47c3c610ac0cab056
Alex73dn <alex73dn@gmail.com>
2023-06-16 12:19:17 +0200

    change a1 React

--- a1.jsx
+++ a1.jsx
@@ -210,3 +210,38 @@ class ToDo extends React.Component {
               <div>
                 <button onClick={this.toggleVisibility}>Click Me</button>
               </div>);}}}
+
+class MyForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: '',
+      submit: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(event) {
+    this.setState({
+      input: event.target.value
+    });
+  }
+  handleSubmit(event) {
+    event.preventDefault();
+    this.setState({
+      submit: this.state.input,
+      input: ''
+    });
+  }
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input value={this.state.input} onChange={this.handleChange} />
+          <button type="submit">Submit</button>
+        </form>
+        <h1>{this.state.submit}</h1>
+      </div>
+    );
+  }
+} // result кнопка submit с вводом и выводом напечатанного текста.
